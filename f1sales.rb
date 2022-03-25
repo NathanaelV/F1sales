03a3f827257512f9109a0cf3fbe954c2384ac879
NathanaelV <tmntsplinter04@gmail.com>
2022-03-25 09:24:58 -0300

    Adicionando teste de falha

--- f1sales.rb
+++ f1sales.rb
@@ -1,16 +1,29 @@
+# site do desafio https://valley-honeycrisp-a14.notion.site/Task-59bc83b5ddd24c0a84f2465c40cc5c02
+
 require 'net/http'
 require 'csv'
 
-puts "Exercicio F1Sales"
+puts "F1Sales Exercise"
 puts
 
-file = File.read('./lost-leads%20(1).csv')
+begin
+    
+  file = File.read('./lost-leads%20(1).csv')
 
-csv = CSV.parse(file, headers: true)
+  csv = CSV.parse(file, headers: true)
 
-uri = URI('https://httpbin.org/post')
+  uri = URI('https://httpbin.org/post')
 
-csv.map do |row|
-  puts Net::HTTP.post_form(uri, row).body
+  csv.each_with_index do |row, i|
+    Net::HTTP.post_form(uri, row)
+    puts "Faltam #{csv.length-i} linhas"
+    # puts Net::HTTP.post_form(uri, row).body
+  end
+  puts 'Fim :)'
   puts
+
+rescue
+  puts "Houve um problema, verifique sua conexão com a internet"
+else
+  puts "Tudo Pronto"
 end
